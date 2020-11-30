afcb273ec63d0645950cca1d40dac81341280628
Juan Peralta <mrperalta@live.com>
2020-11-29 19:53:34 -0500

    adding untracked files

--- week-011/app.py
+++ week-011/app.py
@@ -1,33 +1,32 @@
 from flask import Flask, request, jsonify
-import json
 
 app = Flask(__name__)
 
-DS_Students = {}
+d = {}
 
 @app.route('/', methods=['GET'])
 def get_records():
-    return jsonify(DS_Students)
+    return jsonify(d)
 
 @app.route('/', methods=['POST'])
 def create_record():
     added = {}
     for k,v in request.args.items():
-        if not k in DS_Students.keys():
+        if not k in d.keys():
             added[k] = v
-            DS_Students[k] = v
-    return jsonify({"added": added, "current": DS_Students})
+            d[k] = v
+    return jsonify({"added": added, "current": d})
 
 @app.route('/', methods=['DELETE'])
 def delete_record():
     deleted = {}
     for k,v in request.args.items():
         try:
-            DS_Students.pop(k)
+            d.pop(k)
             deleted[k] = v
         except:
             continue
-    return jsonify({"deleted": deleted, "current": DS_Students})
+    return jsonify({"deleted": deleted, "current": d})
                 
 if __name__ == '__main__':
     app.run(debug=True)
